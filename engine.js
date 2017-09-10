e48c5b861ca24dee866e8dd80b3dccc8ed302dfe
Doug Hatcher <doug.hatcher@blueacorn.com>
2017-09-09 21:18:44 -0400

    combines players and enemy pools for simplicity

--- engine.js
+++ engine.js
@@ -6,6 +6,7 @@ var engine = {
         ness: {
             hp: 50,
             str: 10,
+            type: 'player',
             skills: ['hit']
             
         },
@@ -13,27 +14,28 @@ var engine = {
         paula: {
             hp: 30,
             str: 8,
-
+            type: 'player',
             skills: ['hit', 'heal']
-        }
-    },
+        },
 
-    enemies: {
         1: {
             hp: 10,
             str: 3,
+            type: 'enemy',
             skills: ['hit']
         },
 
         2: {
             hp: 10,
             str: 3,
+            type: 'enemy',
             skills: ['hit']
         },
 
         3: {
             hp: 10,
-            str: 3,            
+            str: 3,   
+            type: 'enemy',         
             skills: ['hit']
 
         }
@@ -48,8 +50,18 @@ var engine = {
 
 
     do: function(src, verb, dest) {
+        
+        if(!this.party[src]) {
+            this.log(src + ' is not in this fight!')
+            return;
+        }
 
         skills = this.party[src]['skills'];
+
+        if(!this.party[dest]) {
+            this.log(dest + ' is not in this fight!')
+            return; 
+        }
 
         msg = src + ' attempts to ' + verb + ' ' + dest + '... '
         match = 0;
@@ -70,7 +82,7 @@ var engine = {
     },
     hit: function(src, dest) {
         dmg = this.party[src].str;
-        this.enemies[dest]['hp'] = this.enemies[dest]['hp'] - dmg;
+        this.party[dest]['hp'] = this.party[dest]['hp'] - dmg;
         this.log(src + ' delievered ' + dmg + ' damage to ' + dest + "!");
     },
 
@@ -82,21 +94,21 @@ var engine = {
 
     status: function() {
         party = 0
+        enemies = 0
         for(char in this.party) {
             if(this.party[char].hp < 0) {
+                delete this.party[char];
                 this.log(char + ' has been killed!');
             }
-            party++;
         }
-        enemies = 0;
-        for(char in this.enemies) {
-            if(this.enemies[char].hp < 0) {
-                delete this.enemies[char];
-                this.log(char + ' has been killed!');
-            }
-            enemies++;
+
+        for(char in this.party) {
+            if(this.party[char].type == "player") player = 1;    
+            if(this.party[char].type == "enemy") enemies = 1;            
         }
-        if(party == 0) this.log('the party was defeated and the enemies robbed them and dragged them back to the checkpoint!')
+
+
+        if(player == 0) this.log('the party was defeated and the enemies robbed them and dragged them back to the checkpoint!')
         if(enemies == 0) this.log('the enemies were defeated!')
 
     },
